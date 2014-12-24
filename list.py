3a519c707088c68d39c6c1c6941a11e1f7c8d74d
Kevin Aloysius <kevinaloys@rocketmail.com>
2014-12-24 01:38:01 -0800

    Airpy first prototype

---
+++ list.py
@@ -0,0 +1,8 @@
+import os
+import airpy
+
+def airlist():
+	installed_docs = os.listdir(airpy.data_directory)
+	for dir in installed_docs:
+		print(dir, end= '  ')
+	print(end = '\n')
---
+++ start.py
@@ -0,0 +1,14 @@
+import click
+import airpy
+import os
+import airpy
+import webbrowser
+
+def airstart(name):
+	directory = airpy.data_directory + '/' + name
+	if not os.path.exists(directory):
+		click.echo('Docs for '+ name + ' is not installed.')
+		click.echo('You can install it by typing, airpy install ' + name)
+	else:
+		static_dir = directory + '/' + name + '-latest' + '/index.html' 
+		return webbrowser.open(static_dir)
