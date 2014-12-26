64c49d8f8deffaeba11a75210d1cdd11e90e3e57
Kevin Aloysius <kevinaloys@rocketmail.com>
2014-12-26 02:28:47 -0800

    Update autopilot.py

--- airpy/autopilot.py
+++ airpy/autopilot.py
@@ -7,10 +7,7 @@ from airpy.install import airinstall
 def airautopilot():
 	installed = subprocess.check_output(['pip','list'])
 	pattern = re.compile('([\w]+)\s\([\d]*\.?[\d]*\.[\d]*\)\\n')
-<<<<<<< HEAD
 	package = pattern.findall(installed.decode('utf-8').lower())
-=======
 	package = pattern.findall(installed.decode('utf-8'))
->>>>>>> master
 	for each_package in package:
 		airinstall(each_package)
