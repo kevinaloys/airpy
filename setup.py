f918baa47f5841cfc63b09ac2307d012592c912b
Kevin Aloysius <kevinaloys@rocketmail.com>
2014-12-25 16:41:01 -0800

    Changed py_modules to package since I want to make airpy a package

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ setup (
 	url = 'https://github.com/kevinaloys/airpy',
 	author = 'Kevin Aloysius',
 	author_email = 'kevinaloysius25@gmail.com',
-	py_modules = ['airpy'],
+	packages = ['airpy'],
 	license = "MIT",
 	install_requires = ['click','requests','appdirs'],
 	entry_points='''
 		[console_scripts]
-		airpy = airpy:airpy
+		airpy = airpy.main:main
 	''',
 
 	)
