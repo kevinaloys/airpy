4c638164c624a6c1d3f2d70cd27ece30020e74d5
Kevin Aloysius <kevinaloys@rocketmail.com>
2014-12-25 16:43:22 -0800

    airpy/main.py
    Inclusion of main.py

---
+++ airpy/main.py
@@ -0,0 +1,55 @@
+import click
+import requests
+import os
+import shutil
+from appdirs import user_data_dir
+from airpy.install import airinstall
+from airpy.list import airlist
+from airpy.start import airstart
+def main():
+	@click.group()
+	def airpy():
+		pass
+
+	@airpy.command()
+	@click.argument('name')
+	def install(name):
+		"""Install offline doc of a Python module."""
+		airinstall(name)
+		
+	@airpy.command()
+	@click.argument('name')
+	def start(name):
+		"""Open the doc in a web browser."""
+		airstart(name)
+
+	@airpy.command()
+	@click.argument('name')
+	def remove(name):
+		"""Remove an installed doc."""
+	
+	@airpy.command()
+	@click.argument('name')
+	def update(name):
+		"""Fetch the latest updated doc."""
+		pass
+
+	@airpy.command()
+	@click.argument('name')
+	def search(name):
+		"""Search for the doc of a Python module."""
+		pass
+	
+	@airpy.command()
+	def list():
+		"""List installed docs."""
+		airlist()
+
+	@airpy.command()
+	def info(name):
+		"""Get Information on a particular doc"""
+
+	airpy()
+
+if __name__ == '__main__':
+	main()
