a4f57133931acdbd17fb81d7d914152f7a78ddc8
Kevin Aloysius <kevinaloys@rocketmail.com>
2014-12-26 02:15:35 -0800

    Make Incoming strings smaller

--- airpy/autopilot.py
+++ airpy/autopilot.py
@@ -7,7 +7,6 @@ from airpy.install import airinstall
 def airautopilot():
 	installed = subprocess.check_output(['pip','list'])
 	pattern = re.compile('([\w]+)\s\([\d]*\.?[\d]*\.[\d]*\)\\n')
-	package = pattern.findall(installed.decode('utf-8'))
-
+	package = pattern.findall(installed.decode('utf-8').lower())
 	for each_package in package:
 		airinstall(each_package)
