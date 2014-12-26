1b359a124882a83929e4798d050b199a5af59946
Kevin Aloysius <kevinaloys@rocketmail.com>
2014-12-26 02:38:00 -0800

    Removed redundant line

--- airpy/autopilot.py
+++ airpy/autopilot.py
@@ -8,6 +8,5 @@ def airautopilot():
 	installed = subprocess.check_output(['pip','list'])
 	pattern = re.compile('([\w]+)\s\([\d]*\.?[\d]*\.[\d]*\)\\n')
 	package = pattern.findall(installed.decode('utf-8').lower())
-	package = pattern.findall(installed.decode('utf-8'))
 	for each_package in package:
 		airinstall(each_package)
