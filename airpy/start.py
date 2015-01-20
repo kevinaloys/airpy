825bb6e59689114d3029991a7817f8f719d1bc03
Kevin Aloysius <kevinaloys@rocketmail.com>
2015-01-20 13:34:07 -0800

    Added entry point for contents.html

--- airpy/start.py
+++ airpy/start.py
@@ -4,10 +4,14 @@ import os
 
 
 def airstart(name):
+	file_extensions = ['index.html','contents.html']
 	directory = airpy.data_directory + '/' + name
 	if not os.path.exists(directory):
 		click.echo('Docs for '+ name + ' is not installed.')
 		click.echo('You can install it by typing, airpy install ' + name)
 	else:
-		static_dir = directory + '/' + name + '-latest' + '/index.html' 
+		for each_extension in file_extensions:
+			if os.path.exists(directory + '/' + name + '-latest' + '/' + each_extension):
+				static_dir = directory + '/' + name + '-latest' + '/' + each_extension
+		
 		return click.launch(static_dir)
