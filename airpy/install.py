d0040a1c3c663deae767cd576c078aa629c125d3
Kevin Aloysius <kevinaloys@rocketmail.com>
2015-01-25 17:53:09 -0800

    Parsing meta json

--- airpy/install.py
+++ airpy/install.py
@@ -4,6 +4,8 @@ import zipfile
 import click
 import io
 import airpy
+import json
+
 from airpy import utils
 
 def airinstall(name):
@@ -11,11 +13,14 @@ def airinstall(name):
 		click.echo('Docs for ' + name + ' is already installed.')
 	else:
 		response = requests.get('https://readthedocs.org/projects/' + name + '/downloads/htmlzip/latest/')
+		json_data = requests.get('https://pypi.python.org/pypi/'+ name + '/json').json()
 		if response.status_code == 200:
 			directory = airpy.data_directory + '/' + name
 			os.makedirs(directory)
 			z = zipfile.ZipFile(io.BytesIO(response.content), 'a')
 			z.extractall(directory)
+			with open(directory + '/' + name + '_airpy.json', 'w') as outfile:
+				json.dump(json_data, outfile, sort_keys = True, indent = 4, ensure_ascii = False)
 			return True
 		else:
 			click.echo('There are no docs for Python module named ' + '\'' + name + '\'')
