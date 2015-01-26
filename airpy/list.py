e7f503f9982897a4f3cefd4ffb086b4c8d47cafb
Kevin Aloysius <kevinaloys@rocketmail.com>
2015-01-25 20:13:17 -0800

    Adjusted the widthe between Project / Summary

--- airpy/list.py
+++ airpy/list.py
@@ -17,12 +17,12 @@ def airlist():
 
 def print_list():
 	installed_docs = airlist()
-	print(Fore.YELLOW + Style.BRIGHT + 'Python Project', end = ' '*40)
+	print(Fore.YELLOW + Style.BRIGHT + 'Python Project', end = ' '*30)
 	print(Fore.YELLOW + Style.BRIGHT + 'Summary')
-	print(Fore.WHITE + Style.DIM + '-'*14, end = ' ' * 32)
+	print(Fore.WHITE + Style.DIM + '-'*14, end = ' ' * 21)
 	print(Fore.WHITE + Style.DIM + '-'*24,)
 	print()
 	for dir in installed_docs:
-		print(Fore.GREEN + Style.BRIGHT + dir + ' '*(46 - len(dir)) + Fore.WHITE + Style.NORMAL + utils.package_info(dir,'summary'))
+		print(Fore.GREEN + Style.BRIGHT + dir + ' '*(35 - len(dir)) + Fore.WHITE + Style.NORMAL + utils.package_info(dir,'summary'))
 	print(end = '\n')
 
