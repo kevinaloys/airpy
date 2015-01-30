75ae6d9c3882f700f595a3008bb23a87cc100a71
Kevin Aloysius <kevinaloys@rocketmail.com>
2015-01-30 01:22:46 -0800

    Created FileNotFoundError from IOError from Python2

--- airpy/utils.py
+++ airpy/utils.py
@@ -2,6 +2,11 @@ import airpy
 import os
 import json
 
+try:
+	FileNotFoundError
+except:
+	FileNotFoundError = IOError
+
 def is_doc_installed(name):
 	directory = airpy.data_directory + '/' + name
 	if os.path.exists(directory):
